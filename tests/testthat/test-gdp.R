2fe74cdcc4d94e3e5193f1c3f906f9347ed00045
Markus Skyttner <mskyttner@pm.me>
2023-11-29 17:15:13 +0100

    add gdp test fcn

--- tests/testthat/test-gdp.R
+++ tests/testthat/test-gdp.R
@@ -1,8 +1,13 @@
+library(swecris)
+library(tidyr)
+library(lubridate)
+library(dplyr)
+library(waldo)
 
 test_that("fundings data match (swecris vs gdp)", {
   # fundings for KTH
 
-  swecris_kth_funding <- swecris_funding()
+  swecris_kth_funding <- swecris_funding("KTH")
 
   # TODO: get GDP data here
   # gdp_kth_funding <- gdp_funding("KTH")
@@ -21,7 +26,8 @@ test_that("fundings data for a specific research area matches (swecris vs gdp)",
   research_area <- "Teknik"
 
   a <-
-    swecris_kth_projects |> unnest_wider("scbs", simplify = TRUE, names_sep = "_") |>
+    swecris_kth_projects |>
+    unnest_wider("scbs", simplify = TRUE, names_sep = "_") |>
     select(-paste0("scbs_", 2:5)) |>
     unnest_longer("scbs_1", values_to = "scb_vals", indices_to = "scb_ind") |>
     filter(scb_vals == research_area)
@@ -79,4 +85,44 @@ test_that("fundings data related to a given coordinator matches (swecris vs gdp)
 
   expect_true(is_equal)
 
+})
+
+
+test_that("data related to ongoing financed activities (swecris vs gdp)", {
+
+
+  orgs <- swecris_organisations()
+  id <- orgs[which(grepl("KTH", orgs$organisationNameSv)), ][1,]
+  swecris_kth_projects <- swecris_projects(id$organisationId)
+
+  # cannot find a VTI-specific API, uses Swecris API for now
+  # cannot find a field with status flags like "ongoing", "finished" etc
+
+  # assumptions:
+  # ongoing: project has begun and has not yet ended
+  a <-
+    swecris_kth_projects |>
+    mutate(
+      beg = lubridate::as_date(strptime(projectStartDate, format = "%Y-%m-%d %H:%M:%S")),
+      end = lubridate::as_date(strptime(projectEndDate, format = "%Y-%m-%d %H:%M:%S"))) |>
+    filter(beg <= Sys.Date() & end <= Sys.Date())
+
+  b <- # TODO: get data from GDP api
+    a #|> bind_rows(a[1,])
+
+  is_equal <- length(compare(a, b)) == 0
+
+  expect_true(is_equal)
+
+})
+
+
+#Filtrera uttaget med Forskningsämne 50102, Tillämpad psykologi
+#Filtrera uttaget med orden (Transport* OR Mobilitet) i (Nyckelord OR Titel OR Beskrivning)
+
+test_that("Test endpoint returns JSON", {
+  res <- gdp::gdp_test()
+  df <- jsonlite::fromJSON(res, simplifyDataFrame = TRUE) |> tibble::as_tibble()
+  is_valid <- nrow(df) == 1 & ncol(df) == 23
+  expect_true(is_valid)
 })
